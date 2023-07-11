4d6d52edfb00c358d505b72f579deda683ea03ec
renato-marciano <remarcia@microsoft.com>
2023-07-11 19:41:01 +0000

    Updated main.tf to produce tf validate error

--- IAC/Terraform/terraform/02_sql/01_deployment/main.tf
+++ IAC/Terraform/terraform/02_sql/01_deployment/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "rg" {
   name     = azurecaf_name.rg_name.result
   location = var.location
 
-  tags = { env : var.env }
+  tags = { env : var.environment }
 }
 
 # ------------------------------------------------------------------------------------------------------
